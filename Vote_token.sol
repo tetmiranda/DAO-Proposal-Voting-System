39a3bcefecdd1f24cbdfa2c7022bd760cf67e658
DKxdc <109942609+DKxdc@users.noreply.github.com>
2023-01-24 18:04:54 +0800

    Update Vote_token.sol

--- Vote_token.sol
+++ Vote_token.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.5.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract ArcadeToken is ERC20, ERC20Detailed {
+contract VoteToken is ERC20, ERC20Detailed {
     address payable owner;
 
     modifier onlyOwner {
