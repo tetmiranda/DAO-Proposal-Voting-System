dc855f60bc72099b424b75cef7261c450efce4bc
DKxdc <109942609+DKxdc@users.noreply.github.com>
2023-01-24 17:19:00 +0800

    reupload

--- Vote_token.sol
+++ Vote_token.sol
@@ -1,18 +1,17 @@
 pragma solidity ^0.5.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/Creepybits/openzeppelin/blob/main/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 
-contract VoteToken is ERC20, ERC20Detailed {
+contract ArcadeToken is ERC20, ERC20Detailed {
     address payable owner;
 
-
     modifier onlyOwner {
-    require(msg.sender == owner, "You do not have permission to mint these tokens!");
-    _;
+        require(msg.sender == owner, "You do not have permission to mint these tokens!");
+        _;
     }
-    
-    constructor(uint initial_supply) ERC20Detailed("Vote Token", "VOTE", 0) public {
+
+    constructor(uint initial_supply) ERC20Detailed("VoteToken", "VOTE", 0) public {
         owner = msg.sender;
         _mint(owner, initial_supply);
     }
@@ -20,6 +19,4 @@ contract VoteToken is ERC20, ERC20Detailed {
     function mint(address recipient, uint amount) public onlyOwner {
         _mint(recipient, amount);
     }
-
-
 }
