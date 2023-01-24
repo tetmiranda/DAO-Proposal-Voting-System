d228f43a1365f3f1c77bab9b6fb7ac8d02522f53
DKxdc <109942609+DKxdc@users.noreply.github.com>
2023-01-24 16:19:50 +0800

    vote token + proposals solidity

---
+++ Proposals.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+
+
+contract Proposal_1 {
+
+  }
+
+
+contract Proposal_2 {
+
+  }
+
+
+contract Proposal_3 {
+
+  }
---
+++ Vote_token.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.5.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/Creepybits/openzeppelin/blob/main/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract VoteToken is ERC20, ERC20Detailed {
+    address payable owner;
+
+
+    modifier onlyOwner {
+    require(msg.sender == owner, "You do not have permission to mint these tokens!");
+    _;
+    }
+    
+    constructor(uint initial_supply) ERC20Detailed("Vote Token", "VOTE", 0) public {
+        owner = msg.sender;
+        _mint(owner, initial_supply);
+    }
+
+    function mint(address recipient, uint amount) public onlyOwner {
+        _mint(recipient, amount);
+    }
+
+
+}
